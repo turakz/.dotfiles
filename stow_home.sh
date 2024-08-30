142b45beb8bffc12bdd1971c559f01eabdbefd6f
turakz <zachary.turak@gmail.com>
2024-08-30 12:38:23 -0400

    adds prompt extension starship

--- stow_home.sh
+++ stow_home.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rm -f "${HOME}/.gtkrc-2.0"  # Some wise-guy keeps replacing my soft-link.
 stow home --no-folding      # Farm soft-links; create directories.
+echo "fractals::environment ~/.config symlinks farmed!"
