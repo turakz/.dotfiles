23bbae6c2536483c74fd9bcc5e119bc5071dfe8b
turakz <zachary.turak@gmail.com>
2024-08-30 12:39:57 -0400

    updates backup

--- backup/home-copy/.config/nvim/init.lua
+++ backup/home-copy/.config/nvim/init.lua
@@ -1,8 +1,17 @@
-require("fractals/options")
+-- note: whenever a module is added or a plugin is changed
+-- the following needs to occur for the configuration change to be recognized
+--[[
+
+  -> packer must be re-compiled, :PackerSync
+  -> .dotfiles repo symlinks need re-farmed so that the new module path is picked up: ./stow_home.sh
+
+]]--
 require("fractals/plugins")
+require("fractals/options")
 require("fractals/autocmds")
 require("fractals/lsp")
 require("fractals/colorscheme")
 require("fractals/editor")
 require("fractals/filebrowsing")
+require("fractals/nvimdap")
 
--- backup/home-copy/.config/nvim/lua/fractals/init.lua
+++
@@ -1,7 +0,0 @@
-require("fractals.options")
-require("fractals.plugins")
-require("fractals.autocmds")
-require("fractals.lsp")
-require("fractals.colorscheme")
-require("fractals.editor")
-require("fractals.filebrowsing")
